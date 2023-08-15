9c9ac4e5f3266bae93613aba9a99f0e92aee60d6
Aztro-dev <davidperezcastellanos@gmail.com>
2023-08-15 13:15:32 -0500

    Weird algorithm read procedure

--- CSES/WeirdAlgorithm/main.asm
+++ CSES/WeirdAlgorithm/main.asm
@@ -1,7 +1,22 @@
 section .text
 global _start
 
+; read(rsi buff, rdx size)
+read:
+  xor rax, rax ; Read
+  xor rdi, rdi ; Stdin
+  syscall
+  ret
+
 _start:
+  mov rsi, buff
+  mov rdx, 3
+  ; read(rsi buff, rdx size)
+  call read
+
   mov rax, 60 ; exit
   mov rdi, 0
   syscall
+
+section .bss
+buff: resb 0x13 ; 19 in decimal
