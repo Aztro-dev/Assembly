d4f9f2deac60a3f1759b6ec8c92002ed5e3af31a
Perez Castellanos <4099238@stumail.dallasisd.org>
2023-08-24 11:18:57 -0500

    I hope WriteFile works

---
+++ USACO/WriteFile/main.asm
@@ -0,0 +1,31 @@
+section .data
+output_file: db "output.out", 0x0
+write_file_mode: dq 0400 ; write access to owner of the file
+
+section .text
+global  _start
+
+_start:
+	mov rax, 0x55 ; creat()
+	mov rdi, output_file
+	mov rsi, qword [write_file_mode]
+	syscall ; file descriptor stored in rax
+	mov qword [file_descriptor], rax
+
+	mov rax, 0x1; write
+	mov rdi, qword [file_descriptor]; Write to file
+	mov rsi, input_buffer
+	mov rdx, 0xE; input_buffer length (16)
+	syscall
+
+	mov     rax, 0x3; close file
+	mov     rdi, qword [file_descriptor]; so the kernel knows what file to close (file_descriptor)
+	syscall ; close file
+
+	mov rax, 60; exit
+	xor rdi, rdi; err_code 0
+	syscall
+
+	section .bss
+	input_buffer: resb 16
+	file_descriptor: resq 1
