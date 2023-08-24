e4c5d4c2d0cb1266989903e70281ccc20f750c12
aztro-dev <davidperezcastellanos@gmail.com>
2023-08-23 21:33:04 -0500

    Reading from a file works now!

--- USACO/ReadFile/main.asm
+++ USACO/ReadFile/main.asm
@@ -1,29 +1,37 @@
-struc kernel_timespec
-tv_sec: resq 1
-tv_nsec: resq 1
-endstruc
+section .data
+input_file: db "input.in", 0x0
 
 section .text
 global  _start
 
 _start:
-	mov rax, 35; nanosleep
-	mov rdi, qword [request]
-	mov rsi, qword [remaining]
+	mov     rax, 0x02; open
+	mov     rdi, input_file
+	xor     rsi, rsi; no idea what int flags do
+	xor     rdx, rdx; no idea what umode_t mode does
+	syscall ; call open and return file descriptor in rax
+	mov     qword [file_descriptor], rax; store file descriptor for later use
+
+	mov rdi, qword [file_descriptor]; file descriptor
+	xor rax, rax; read
+	mov rsi, input_buffer; buffer
+	mov rdx, 0x10; input_buffer length (16)
 	syscall
 
-	mov     rax, 60; exit(
-	xor     rdi, rdi; err_code: 0
-	syscall ; )
+	mov rax, 0x1; write
+	mov rdi, 0x1; stdout
+	mov rsi, input_buffer
+	mov rdx, 0xE; input_buffer length (16)
+	syscall
 
-	section .data:
+	mov     rax, 0x3; close file
+	mov     rdi, qword [file_descriptor]; so the kernel knows what file to close (file_descriptor)
+	syscall ; close file
 
-request:
-	istruc kernel_timespec
-	at     tv_sec, dq 0x5
-	at     tv_nsec, dq 100
-	iend
+	mov rax, 60; exit
+	xor rdi, rdi; err_code 0
+	syscall
 
-remaining:
-	istruc kernel_timespec
-	iend
+	section .bss
+	input_buffer: resb 16
+	file_descriptor: resq 1
