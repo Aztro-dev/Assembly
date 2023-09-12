adc1fcbed2d70d6c58142115b86f6e1c73a0669c
Aztro-dev <davidperezcastellanos@gmail.com>
2023-09-12 13:32:23 -0500

    Input each number for parameters

--- LinkedASM/x86_64/Parameters/main.cpp
+++ LinkedASM/x86_64/Parameters/main.cpp
@@ -1,14 +1,12 @@
-#include <stdio.h>
+#include <iostream>
 #include <cstdint>
 
 extern "C" uint64_t parameters(uint64_t a, uint64_t b, uint64_t c, uint64_t d);
 
 int main(){
-  uint64_t a = 100;
-  uint64_t b = 200;
-  uint64_t c = 300;
-  uint64_t d = 400;
+  uint64_t a, b, c, d;
+  std::cin >> a >> b >> c >> d;
   uint64_t result = parameters(a, b, c, d);
-  printf("%lld\n", result);
+  std::cout << result << std::endl;
   return 0;
 }
--- LinkedASM/x86_64/Parameters/parameters.asm
+++ LinkedASM/x86_64/Parameters/parameters.asm
@@ -3,6 +3,6 @@ global  parameters
 
 parameters:
 	add rax, rdx; a + b
-	add rax, r8 ; (a + b) + c
-	add rax, r9 ; (a + b) + (c + d)
+	add rax, r8; (a + b) + c
+	add rax, r9; (a + b) + (c + d)
 	ret ; return rax
