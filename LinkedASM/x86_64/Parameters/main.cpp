88839335c3dc6acbd52a6ec6ab941b0cb1509d3b
Aztro-dev <davidperezcastellanos@gmail.com>
2023-09-12 12:43:22 -0500

    More parameters

--- LinkedASM/x86_64/Parameters/main.cpp
+++ LinkedASM/x86_64/Parameters/main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <cstdint>
 
-extern "C" uint64_t parameters(uint64_t a, uint64_t b);
+extern "C" uint64_t parameters(uint64_t a, uint64_t b, uint64_t c, uint64_t d);
 
 int main(){
   uint64_t a = 100;
   uint64_t b = 200;
-  uint64_t result = parameters(a, b);
+  uint64_t c = 300;
+  uint64_t d = 400;
+  uint64_t result = parameters(a, b, c, d);
   printf("%lld\n", result);
   return 0;
 }
--- LinkedASM/x86_64/Parameters/parameters.asm
+++ LinkedASM/x86_64/Parameters/parameters.asm
@@ -3,4 +3,6 @@ global  parameters
 
 parameters:
 	add rax, rdx; a + b
+	add rax, r8 ; (a + b) + c
+	add rax, r9 ; (a + b) + (c + d)
 	ret ; return rax
