4717c56936162f860c10a0804efaba49cbe5904e
Aztro-dev <davidperezcastellanos@gmail.com>
2023-09-13 15:43:13 -0500

    Name fix

---
+++ LinkedASM/x86_64/windows/Parameters/parameters.asm
@@ -0,0 +1,11 @@
+section .note.GNU-stack
+
+section .text
+global  parameters
+
+parameters:
+	add rcx, rdx; a + b
+	add rcx, r8; (a + b) + c
+	add rcx, r9; (a + b) + (c + d)
+	mov rax, rcx; store result in rax to return
+	ret ; return rax
