0e3c4e73ee62b63cd9d1ca8e28d5b0c656441c00
aztro-dev <davidperezcastellanos@gmail.com>
2023-09-17 20:32:35 -0500

    ATOI still not done yet :(

--- Functions/x86_64/linux/atoi/atoi.asm
+++ Functions/x86_64/linux/atoi/atoi.asm
@@ -11,8 +11,7 @@ atoi_asm:
 	;     int64_t atoi(const char* buffer)
 	call  strlen; length (in elements) stored in rax
 	mov   rcx, rax; rcx will be the iterator
-	mov   rsi, rdi
-	mov   al, byte[rsi]; result stored in al
+	mov   al, byte[rdi]; result stored in al
 	cmp   al, '-'; if number is negative
 	cmove r8w, dx; set sign flag to 1
 	xor   rdx, rdx; clear rdx (output)
@@ -24,11 +23,11 @@ atoi_asm:
 	jle .exit
 	dec rcx
 
-	mov al, byte[rsi]; result stored in al
-	sub al, '0'; ascii char to int
+	mov al, byte[rdi]; result stored in al
+	sub al, 48; ascii char to int
 	add rdx, rax
 
-	inc rsi; next byte
+	inc rdi; next byte
 
 	jmp .loop
 
--- Functions/x86_64/linux/atoi/main.cpp
+++ Functions/x86_64/linux/atoi/main.cpp
@@ -5,7 +5,8 @@
 extern "C" int64_t atoi_asm(const char *s);
 
 int main() {
-  const char *s = "1234";
+  const char *s = "1234567";
+  printf("%s\n", s);
   int64_t result = atoi_asm(s);
   printf("%lld\n", result);
   return 0;
