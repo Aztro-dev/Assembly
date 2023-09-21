4501e7fc9788b8d017ece510390b87417b4fb78e
Aztro-dev <davidperezcastellanos@gmail.com>
2023-09-21 10:16:17 -0500

    Printf in windows

--- Tests/WindowsASM/main.asm
+++ Tests/WindowsASM/main.asm
@@ -1,9 +1,18 @@
 section .text
 global  main
 extern  ExitProcess
+extern  printf
 
 main:
+	push rbp; use 8 bytes of stack space in order to align the stack
+	mov  rcx, format_str; format string (first arg)
+	mov  rdx, 21; Integer to print (second arg)
+	mov  al, 0; magic for varargs (0 = no magic; prevents crash)
+	call printf
 	;    ExitProcess(0)
 	mov  rcx, 0
 	call ExitProcess
 	ret
+
+section .data
+format_str: db "bruh: %d", 0x0a, 0 ; "bruh: %d\n\0"
