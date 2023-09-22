44a9645a4fc465b437adf5a17fd058dafd07a3bf
Aztro-dev <davidperezcastellanos@gmail.com>
2023-09-22 13:09:39 -0500

    relocation thingy :(

--- Tests/WindowsASM/main.asm
+++ Tests/WindowsASM/main.asm
@@ -2,11 +2,17 @@ section .text
 global  main
 extern  ExitProcess
 extern  printf
+extern  scanf
 
 main:
+	push rbp ; align stack
+	mov rcx, input_fmt_str ; format string
+	mov rdx, input ; input pointer
+	call scanf
+
 	push rbp; use 8 bytes of stack space in order to align the stack
 	mov  rcx, format_str; format string (first arg)
-	mov  rdx, 21; Integer to print (second arg)
+	mov  rdx, qword[input]; Integer to print (second arg)
 	mov  al, 0; magic for varargs (0 = no magic; prevents crash)
 	call printf
 	;    ExitProcess(0)
@@ -16,3 +22,7 @@ main:
 
 section .data
 format_str: db "bruh: %d", 0x0a, 0 ; "bruh: %d\n\0"
+input_fmt_str: db "Input: %lld", 0x0a, 0 ; "Input: %lld\n\0"
+
+section .bss ; variables
+input: resq 1 ; uint64_t
