bb448525dafefcfec7e38892b55709b90e1d9d4f
Aztro-dev <davidperezcastellanos@gmail.com>
2023-09-25 12:34:15 -0500

    Get rid of capitalization of registers

--- Tests/ARM-Boilerplate/main.asm
+++ Tests/ARM-Boilerplate/main.asm
@@ -1,5 +1,5 @@
-// X0-X2 - parameters to linux function services
-// X16 - linux function number
+// x0-x2 - parameters to linux function services
+// x16 - linux function number
 //
 .global _main             // Provide program starting address to linker
 .align 2
@@ -8,17 +8,17 @@
 // and then call Linux to do it.
 
 _main: 
-  mov X0, #1     // 1 = stdout
-  adr X1, helloworld // string to print
-  mov X2, #13     // length of our string
-  mov X16, #4     // MacOS write system call
+  mov x0, #1     // 1 = stdout
+  adr x1, helloworld // string to print
+  mov x2, #13     // length of our string
+  mov x16, #4     // MacOS write system call
   svc 0     // Call linux to output the string
 
 // Setup the parameters to exit the program
 // and then call Linux to do it.
 
-  mov     X0, #0      // Use 0 return code
-  mov     X16, #1     // Service command code 1 terminates this program
+  mov     x0, #0      // Use 0 return code
+  mov     x16, #1     // Service command code 1 terminates this program
   svc     0           // Call MacOS to terminate the program
 
 helloworld:      .ascii  "Hello World!\n"
