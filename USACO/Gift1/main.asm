70f995b814d7729b098dfd3c76b37308dbd4319f
aztro-dev <davidperezcastellanos@gmail.com>
2023-09-30 21:27:35 -0500

    Still dumb as hell

--- USACO/Gift1/main.asm
+++ USACO/Gift1/main.asm
@@ -12,31 +12,31 @@ solve:
 	ret
 
 _start:
-	; mov     rax, 0x02; open
-	; mov     rdi, input_file
-	; xor     rsi, rsi; no idea what int flags do
-	; xor     rdx, rdx; no idea what umode_t mode does
-	; syscall; call open and return file descriptor in rax
-	; mov     qword [input_file_descriptor], rax; store file descriptor for later use
+	mov      rax, 0x02; open
+	mov      rdi, input_file
+	xor      rsi, rsi; no idea what int flags do
+	xor      rdx, rdx; no idea what umode_t mode does
+	syscall; call open and return file descriptor in rax
+	mov      qword [input_file_descriptor], rax; store file descriptor for later use
 
-	; mov rdi, qword [input_file_descriptor]; file descriptor
-	; xor rax, rax; read
-	; mov rsi, input_buffer; buffer
-	; mov rdx, 0xE; input_buffer length (14)
-	; syscall
+	mov rdi, qword [input_file_descriptor]; file descriptor
+	xor rax, rax; read
+	mov rsi, input_buffer; buffer
+	mov rdx, 0xE; input_buffer length (14)
+	syscall
 
-	; mov     rax, 0x55; creat()
-	; mov     rdi, output_file
-	; mov     rsi, qword [write_file_mode]
-	; syscall; file descriptor stored in rax
-	; mov     qword [output_file_descriptor], rax
+	mov      rax, 0x55; creat()
+	mov      rdi, output_file
+	mov      rsi, qword [write_file_mode]
+	syscall; file descriptor stored in rax
+	mov      qword [output_file_descriptor], rax
 
-	; ; input text should be stored in input_buffer
-	; call solve
+	;    input text should be stored in input_buffer
+	call solve
 
-	; mov     rax, 0x3; close file
-	; mov     rdi, qword [input_file_descriptor]; so the kernel knows what file to close (input_file_descriptor)
-	; syscall; close file
+	mov      rax, 0x3; close file
+	mov      rdi, qword [input_file_descriptor]; so the kernel knows what file to close (input_file_descriptor)
+	syscall; close file
 
 	mov rax, 60; exit
 	xor rdi, rdi; err_code 0
