e56b0690def84914f76f04bf63645a57feca05d7
aztro-dev <davidperezcastellanos@gmail.com>
2023-10-29 23:05:41 -0500

    Trying to put the numbers into an array, but I can't validate whether or not they're in there

--- USACO/Gift1/main.asm
+++ USACO/Gift1/main.asm
@@ -2,22 +2,24 @@ section .data
 input_file db "gift1.in", 0x0
 output_file db "gift1.out", 0x0
 write_file_mode dq 0400 ; write access to owner of the file
+newline db 0xA
 
 section .bss
-input_buffer resb 14
 integer_buffer resb 19 ; To store a 64-bit integer
 integer_buffer_len equ $ - integer_buffer
 name_buffer resb 14
 name_buffer_len equ $ - name_buffer
-input_character resw 1
+input_name resb 14
 
-NP:
-	resq 1
-	input_file_descriptor: resq 1
-	output_file_descriptor: resq 1
+bank_array resq 10 ; each person should have a bank account, and there is a max of 10 people
+names_array times 10 resb 14 ; each person has a name, and there is a max of 10 people
 
-	section .text
-	global  _start
+np resq 1
+input_file_descriptor resq 1
+output_file_descriptor resq 1
+
+section .text
+global  _start
 
 atoi:
 	push rbx; if you are going to use rbx you must preserve it by pushing it onto the stack
@@ -53,6 +55,48 @@ atoi:
 	pop rbx; restore rbx to its original value
 	ret ; number is in rax
 
+	; void print_itoa(rdi n)
+
+print_itoa:
+	;   Clear these two registers for division
+	mov rax, rdi; rax = n
+	xor rdx, rdx
+
+	xor r9, r9; Digits count
+
+	mov r10, 10; Divison by 10
+
+	mov rsi, integer_buffer; To put the integer in
+	cmp rdi, 0
+	jge .loop
+	;   Integer should be negative after this point and before positive
+	mov byte [rsi], '-'
+	;   Back to positive numbers
+	neg rax
+
+	inc rsi
+	inc r9
+
+.loop:
+	test rax, rax
+	jz   .print
+	xor  rdx, rdx; reset quotient
+	div  r10; next digit stored in rdx
+	add  rdx, 48; digit + 48 = digit in ascii
+	mov  byte[rsi], dl; digit stored into next byte
+	inc  rsi; Next byte
+	inc  r9; Length
+
+	jmp .loop
+
+.print:
+	mov rax, 0x1; Write
+	mov rdi, [output_file_descriptor]; Da file
+	mov rsi, [integer_buffer]
+	mov rdx, r9; Length
+	syscall
+	ret
+
 solve:
 	xor rax, rax; Read
 	mov rdi, [input_file_descriptor]; Da file
@@ -70,11 +114,13 @@ solve:
 .exit_read_np_loop:
 	mov  rdi, integer_buffer
 	call atoi; Output stored in rax
-	mov  qword [NP], rax; Store the number into NP
+	mov  qword [np], rax; Store the number into np
 
 	mov r10, rax; rcx will be the name counter
 
 	mov r8, rsp; Make sure not to lose track of rsp!
+
+	xor r12, r12; person index
 
 .read_names_loop:
 	cmp r10, 0
@@ -84,36 +130,45 @@ solve:
 	xor r9, r9; Store length of name
 
 	mov rdi, [input_file_descriptor]; Da file
-	mov rsi, input_character; Yikes this is risky :|
+	mov rsi, input_name
 	mov rdx, 0x1
 
 .read_name:
-	xor  rax, rax; Read
+	xor rax, rax; Read
 	syscall
-	cmp  byte [rsi], 0xA; Check if at newline
-	je   .exit_read_name
-	push word [input_character]
-	inc  r9; Increase length of string
-	jmp  .read_name
+	cmp byte [rsi], 0xA; Check if at newline
+	je  .exit_read_name
+	inc rsi; Next byte in input_name
+	inc r9; Increase length of string
+	jmp .read_name
 
 .exit_read_name:
-	add rsp, r9; To read at the beginning of the string
-	mov rax, 0x1; Write
-	mov rdi, [output_file_descriptor]; Da file
-	mov rsi, rsp
-	mov rdx, r9
-	syscall
+	inc r12; person index
 
-	sub rsp, r9; Go back after the string to add the bank balance
-
-	xor  rax, rax
-	push rax
+	mov rsi, bank_array
+	add rsi, r12; index into ith person's bank account
+	mov qword [rsi], r12
 
 	jmp .read_names_loop
 
 .exit_read_names_loop:
 	mov rsp, r8; Restore rsp
 
+	mov r13, bank_array
+	mov rdi, qword [bank_array]
+
+.print_bank_accounts:
+	test r12, r12
+	jz   .exit_print_bank_accounts
+	dec  r12
+
+	call print_itoa; void print_itoa(rdi n)
+	add  r13, 0x8; Next qword
+	mov  rdi, qword [r13]
+
+	jmp .print_bank_accounts
+
+.exit_print_bank_accounts:
 	ret
 
 _start:
