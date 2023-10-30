46bd4789cdd9900c6011e55cbd005ffa84ac28b6
Aztro-dev <davidperezcastellanos@gmail.com>
2023-10-30 14:48:32 -0500

    I was using linux parameters in windows lmfao

--- Tests/Min/min.asm
+++ Tests/Min/min.asm
@@ -3,13 +3,13 @@ global asm_min
 
 ; rax min(rdi num1, rsi num2);
 asm_min:
-  mov rax, rdi
-  sub rax, rsi
+  mov rax, rcx
+  sub rax, rdx
   ; abs(x) = (x ^ y) - y
   ; y = x >> 63
   mov rbx, rax
   shr rbx, 63
   xor rax, rbx
   sub rax, rbx
-  add rax, rsi
+  add rax, rdx
   ret
