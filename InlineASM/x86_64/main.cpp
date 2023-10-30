ad82d31899c059d7a45c3485237f4aaf57a81a6f
Aztro-dev <davidperezcastellanos@gmail.com>
2023-10-30 11:44:18 -0500

    Formatting and makefile changes

--- InlineASM/x86_64/main.cpp
+++ InlineASM/x86_64/main.cpp
@@ -1,27 +1,24 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 int foo(void) {
-  uint64_t joe=1234, bob=4321, fred;
-  __asm__( 
-"  mov %0, %1\n"
-"  add %0, %2\n"
-:"=r" (fred) // =r means write to fred*/
-:"r" (joe), "r" (bob) /* r means use it as a variable*/
-:"rax" /* Overwrite */
+  uint64_t joe = 1234, bob = 4321, fred;
+  __asm__("  mov %0, %1\n"
+          "  add %0, %2\n"
+          : "=r"(fred)         // =r means write to fred*/
+          : "r"(joe), "r"(bob) /* r means use it as a variable*/
   );
   return fred;
 }
 
 extern "C" uint64_t proc(uint64_t a); /* Prototype */
 
-__asm__( /* Assembly function body */
-"proc:\n"
-"  mov rax, 0x100\n"
-"  ret\n"
-);
+__asm__(/* Assembly function body */
+        "proc:\n"
+        "  mov rax, 0x100\n"
+        "  ret\n");
 
-int main(void){
+int main(void) {
   int output = foo();
   uint64_t proc_output = proc(10);
 
