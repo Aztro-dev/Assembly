bb2efa278e724dfc83aaf45170c8d48f58d8d4b8
Aztro-dev <davidperezcastellanos@gmail.com>
2023-11-03 12:55:19 -0500

    Absolute value is working now

--- Tests/Min/main.c
+++ Tests/Min/main.c
@@ -4,6 +4,6 @@ extern long long int asm_min(long long int num1, long long int num2);
 
 int main(void) {
   long long int result = asm_min(2, 5);
-  printf("%lld", result);
+  printf("%lld\n", result);
   return 0;
 }
--- Tests/Min/min.asm
+++ Tests/Min/min.asm
@@ -1,15 +1,32 @@
 section .text
 global asm_min
 
-; rax min(rdi num1, rsi num2);
+
+
+; rax min(rcx num1, rdx num2);
 asm_min:
   mov rax, rcx
   sub rax, rdx
-  ; abs(x) = (x ^ y) - y
+
+  .abs:
+
+;mov bx,ax
+;add bx,bx
+;sbc bx,bx
+;xor ax,bx
+;sub ax,bx
+mov rbx, rax
+add rbx, rbx
+sbb rbx, rbx
+xor rax, rbx
+sub rax, rbx
+  
   ; y = x >> 63
-  mov rbx, rax
-  shr rbx, 63
-  xor rax, rbx
-  sub rax, rbx
+  ; abs(x) = (x ^ y) - y
+  ; mov rbx, rax
+  ; shr rbx, 63
+  ; xor rbx, rax
+  ; sub rax, rbx
+
   add rax, rdx
   ret
