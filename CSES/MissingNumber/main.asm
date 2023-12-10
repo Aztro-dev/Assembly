580b73d5af505f3daab06d1d0c89a44bba20da5b
aztro-dev <davidperezcastellanos@gmail.com>
2023-12-09 21:51:18 -0600

    Don't forget to save changes

--- CSES/MissingNumber/main.asm
+++ CSES/MissingNumber/main.asm
@@ -1,36 +1,38 @@
-section .bss
+section .data
+uint64_buffer db 20 dup(0x0) ; 20 Digits in uint64_t
 
-n:
-	resb 14
+n dq 1
 
-	section .text
-	global  _start
+section .text
+global  _start
 
+read_int:
+    mov rdi, 0x0 ; STDIN
+    mov rsi, uint64_buffer
+    mov rdx, 0x1 ; One character at a time
+    .read_loop:
+        xor rax, rax ; READ syscall
+        syscall
+        cmp byte [rsi], 0x0 ; Null character
+        je .exit_read_loop
+        cmp byte [rsi], 0x0a ; newline
+        je .exit_read_loop
+        sub rsi, 20
+        cmp rsi, uint64_buffer
+        je .exit_read_loop
+        add rsi, 21 ; Reset to previous value and increment
+        jmp .read_loop
+    .exit_read_loop:
+    mov rdi, uint64_buffer
+    ret
 _start:
-	xor rax, rax; READ
-	xor rdi, rdi; STDIN
-	mov rsi, n; numbers
-	mov rdx, 0x1; 1 character at a time
-	mov r8, 0x1; Character count
-	syscall
-
-.read_n:
-	cmp byte [rsi], 0x0a; Newline
-	je  .exit_read_n
-	inc rsi; Next byte in n
-	inc r8
-	mov rdx, 0x1
-	syscall
-	jmp .read_n
-
-.exit_read_n:
-	mov rax, 0x1; WRITE
-	mov rdi, 0x1; STDOUT
-	mov rsi, n; Thing we just inputted :|
-	mov rdx, r8
-	syscall
+	call read_int
+	mov qword [n], rax
+	ret
 
 	mov rax, 60; exit
 	xor rdi, rdi; Error code 0
 	syscall
 	ret
+
+
