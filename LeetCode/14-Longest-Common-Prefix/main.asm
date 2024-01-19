93fc7ed7ed1dbda45af469d0c959ad600188d91a
Aztro-dev <davidperezcastellanos@gmail.com>
2024-01-19 10:20:11 -0600

    Problem solved

--- LeetCode/14-Longest-Common-Prefix/main.asm
+++ LeetCode/14-Longest-Common-Prefix/main.asm
@@ -1,5 +1,5 @@
 section .data
-    strs db "flower", 0x0, "flow", 0x0, "flight", 0x0
+    strs db "flight", 0x0, "flower", 0x0, "flow", 0x0
     strs_len equ $ - strs
 
 section .text
@@ -8,7 +8,6 @@ section .text
 longestCommonPrefix:
     xor rax, rax
     xor r8, r8 ; Index for all strings
-    ;mov r8, 0x0 ; Index for all strings
     .loop:
         mov rsi, strs
         add rsi, r8
@@ -18,15 +17,15 @@ longestCommonPrefix:
         
         mov rcx, r8 ; Iterator starts at index
         .check_next_string:
-            cmp rcx, strs_len
-            jge .after_check_next_string
-            
             .while_not_next_string:
                 inc rcx
                 mov al, byte [strs + rcx]
                 cmp al, 0x0
                 jne .while_not_next_string
             inc rcx
+            cmp rcx, strs_len
+            jge .after_check_next_string
+            
             mov al, byte [strs + r8 + rcx]
             cmp al, ah ;If the current character is not equal to desired character
             jne .exit
