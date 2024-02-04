2c29edf42555c5f60c465d0b17a8ccb1bd1b2135
aztro-dev <davidperezcastellanos@gmail.com>
2024-02-03 23:51:32 -0600

    .note.GNU-stack

--- Tests/Min/min.asm
+++ Tests/Min/min.asm
@@ -1,32 +1,33 @@
 section .text
-global asm_min
+global  asm_min
 
+	; rax min(rcx num1, rdx num2)
 
-
-; rax min(rcx num1, rdx num2);
 asm_min:
-  mov rax, rcx
-  sub rax, rdx
+	mov rax, rcx
+	sub rax, rdx
 
-  .abs:
+.abs:
 
-;mov bx,ax
-;add bx,bx
-;sbc bx,bx
-;xor ax,bx
-;sub ax,bx
-mov rbx, rax
-add rbx, rbx
-sbb rbx, rbx
-xor rax, rbx
-sub rax, rbx
-  
-  ; y = x >> 63
-  ; abs(x) = (x ^ y) - y
-  ; mov rbx, rax
-  ; shr rbx, 63
-  ; xor rbx, rax
-  ; sub rax, rbx
+	;mov bx, ax
+	;add bx, bx
+	;sbc bx, bx
+	;xor ax, bx
+	;sub ax, bx
+	mov  rbx, rax
+	add  rbx, rbx
+	sbb  rbx, rbx
+	xor  rax, rbx
+	sub  rax, rbx
 
-  add rax, rdx
-  ret
+	; y = x >> 63
+	; abs(x) = (x ^ y) - y
+	; mov rbx, rax
+	; shr rbx, 63
+	; xor rbx, rax
+	; sub rax, rbx
+
+	add rax, rdx
+	ret
+
+section .note.GNU-stack
