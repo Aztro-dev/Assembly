106962b2f6dae72b180151d763e482fec1bc91cb
aztro-dev <davidperezcastellanos@gmail.com>
2024-03-10 23:30:31 -0500

    IncreasingArray solved, but not producing result on CSES website

--- CSES/IncreasingArray/main.asm
+++ CSES/IncreasingArray/main.asm
@@ -1,181 +1,212 @@
 section .data
+number_buffer times 20 db 0 ; 20 Digits in uint64_t
+dummy_n       dq 0x0
+n       dq 0x0
 
-space:
-	db " "
+section .text
+global  _start
 
-	section .text
-	global  _start
-
-	; read(rsi input, rdx size)
-
-read:
-	xor rax, rax; Read
-	xor rdi, rdi; Stdin
-	syscall
+solve:
+	;   if(nums.size() == 1){ return 0; }
+	mov rax, qword[n]
+	cmp rax, 0x1
+	jg  .not_length_of_one
+	xor rax, rax
 	ret
 
-	; print(rsi buff, rdx length)
+.not_length_of_one:
+	xor rax, rax; sum
+	mov r8, 0x1; i
 
-print:
-	mov rax, 1; write
-	mov rdi, 1; stdout
-	syscall
-	ret
+	;   Reset high parts of registers
+	;   Because movzx doesnt work apparently
+	xor rbx, rbx
+	xor rcx, rcx
 
-	; atoi (rsi pointer_to_ascii) -> rdx
+	; for(int i = 1; i < nums.size(); i++)
 
-atoi:
-	xor   rax, rax; rax = 0
-	xor   rdx, rdx; rdx = 0
-	lodsb ; load byte at address RSI into AL.
-	cmp   al, '-'
-	sete  bl; bl = negative flag
-	jne   .lpv; jump to .lpv if positive
+.loop:
+	cmp r8, qword[n]
+	je  .exit_loop
 
-.lp:
-	lodsb ; load byte at address RSI into AL
+	;nums[i]
+	lea rdi, [arr + 4*r8]
+	mov ebx, dword [rdi]
+	;   nums[i - 1]
+	lea rdi, [arr + 4*r8 - 4]
+	mov ecx, dword [rdi]
 
-.lpv:
-	sub  al, '0'; turns al into number
-	jl   .end; if sign flag != overflow flag, jump to .end
-	imul rdx, 10; rdx *= 10
-	add  rdx, rax; rdx += rax
-	jmp  .lp; load byte at address RSI into AL
+	;   if (nums[i] < nums[i - 1])
+	cmp ebx, ecx
+	jg  .continue
 
-.end:
-	test bl, bl; bl & bl
-	jz   .p; if bl is zero, return
-	neg  rdx; rdx = -rdx
-	;    xchg rax, rdx
+	add rax, rcx; sum += nums[i - 1]
+	sub rax, rbx; sum -= nums[i]
+	lea rdi, [arr + 4*r8]
+	mov dword [rdi], ecx; nums[i] = nums[i - 1]
 
-.p:
-	ret
+.continue:
+	inc r8
+	jmp .loop
 
-	; itoa(rax integer) -> rsi output
-
-itoa:
-	std  ; rsi -= 1, rdi -= 1
-	mov  r9, 10; r9 will be our base (base 10)
-	bt   rax, 63; copy bit 63 (most significant, handles negatives) from rax to carry flag
-	setc bl; if carry flag is set, set bl to 1 (negative sign)
-	jnc  .lp; if carry flag isn't set, jump to .lp
-	neg  rax; if carry flag is set, negate the number
-
-.lp:
-	xor   rdx, rdx; reset rdx
-	div   r9; rax / r9 -> rax output, rdx remainder
-	xchg  rax, rdx; swap rax and rdx
-	add   rax, '0'; rax is now the remainder, which is turned into an ascii integer
-	stosb ; load byte at rsi into al
-	xchg  rax, rdx; rax is now the result of the first division, and rdx is now the ?
-	test  rax, rax; if rax is zero
-	jnz   .lp; if rax isn't zero, jump to .lp (loop), this ensures we have another digit to process
-	test  bl, bl; if bl is zero (sign or no sign)
-	jz    .p; if number is positive, jump to .p (exit)
-	mov   al, '-'; if number is NOT positive (negative), add a negative sign
-	stosb ; load byte at rsi into al
-
-.p:
-	cld ; clear direction flag (Turns off automatic rsi increments whenever calling a string function (like stosb))
-	inc rdi
-	ret
-
-	; print_itoa(rsi buff, rax input) -> void
-
-print_itoa:
-	call itoa
-	sub  rsi, rdi
-	mov  rdx, rsi
-	mov  rsi, rdi
-	;    print(rsi buff, rdx length)
-	call print
-
-print_space:
-	mov  rsi, space
-	mov  rdx, 0x1; length of one
-	call print
-
-	; atoi(rsi input) -> rcx
-
-atoi_rcx:
-	mov   rsi, input
-	call  atoi; atoi(rsi input) -> rdx
-	mov   rcx, rdx; store value of atoi
-	mov   rdi, input+18; end of buff
-	mov   rsi, rdi; rsi = rdi
-	std   ; rsi--, rdi--
-	mov   rax, 10
-	stosb ; al = [rsi]
-	ret
-
-malloc:
-	;   malloc(rdi n) -> rax ptr_to_array
-	mov rax, 0xC; 12 in decimal (brk)
-	syscall
-	ret
-
-IncreasingArray:
-	;   IncreasingArray() -> rax output
-	xor r8, r8; current number
-	xor r9, r9; previous number
-
-	xor rax, rax; set rax to zero for the output
-	cmp rcx, 0x1; If there is only one number, then we don't need to do anything
-	je  .exit
-
-	xor r11, r11; iterator i
-	mov rsi, arr; rsi is the pointer to the array
-
-	.set_arr_loop: ; for(int i = 0; i < n; i++){
-	cmp r11, rcx; See if i is less than n
-	jge .after_set_arr_loop
-	inc r11; i++
-
-	;    atoi_rcx(rsi input) -> rcx output
-	call atoi_rcx
-
-	jmp .set_arr_loop
-
-.after_set_arr_loop:
-	xor rax, rax; reset rax
-	mov rcx, qword [n]; restore n back into rcx
-	mov rsi, arr; Go back to the start of the array
-
-.exit:
+.exit_loop:
 	ret
 
 _start:
-	mov  rsi, n_str
-	mov  rdx, 19
-	;    read(rsi input, rdx size)
-	call read
+	call read_int
+	mov  qword [n], rax
 
-	;    atoi_rcx(rsi input) -> rcx output
-	call atoi_rcx
-
-	mov qword[n], rcx
-
-	mov  rdi, rcx
-	call malloc; output ptr stored in rax
-	mov  arr, rax; arr now points to the start of the array
-
-	;    IncreasingArray() -> rax output
-	call IncreasingArray
-
-	;    print_itoa(rax input)
-	call print_itoa; print the output of IncreasingArray
-
-	mov rax, 60; exit
+	;   brk(0)
+	mov rax, 12
 	mov rdi, 0
 	syscall
 
-	section .bss
+	mov qword[arr], rax
 
-n_str:
-	resb 0x13; 19 in decimal
+	;   brk(brk(0) + n * 4)
+	mov rax, qword[n]
+	mov r8, 0x4
+	mul r8
+	mov rdi, rax
+	add rdi, qword[arr]
+	mov rax, 12
+	syscall
 
-n:
-	resq 0x1; 1 in decimal
+	mov r8, qword [n]
+	mov rdi, arr
 
-arr:
-	resq 0x0; pointer to array
+.populate_array_loop:
+	test r8, r8
+	jz   .exit_populate_array_loop
+
+	push rdi
+	call read_int
+	pop  rdi
+
+	mov dword[rdi], eax
+	add rdi, 0x4; Next int
+
+	dec r8
+	jmp .populate_array_loop
+
+.exit_populate_array_loop:
+	call solve
+
+	mov  rdi, rax
+	call print_uint64
+
+	mov rax, 60; Exit
+	mov rdi, 0x0; Exit Code 0
+	syscall
+
+read_int:
+	mov rdi, 0x0; STDIN
+	mov rsi, number_buffer
+	mov rdx, 0x1; One character at a time
+
+.read_loop:
+	xor rax, rax; READ syscall
+	syscall
+	cmp byte [rsi], 0x0; Null character
+	je  .exit_read_loop
+	cmp byte [rsi], 0x0a; newline
+	je  .exit_read_loop
+	sub rsi, 20
+	cmp rsi, number_buffer
+	je  .exit_read_loop
+	add rsi, 21; Reset to previous value and increment
+	jmp .read_loop
+
+.exit_read_loop:
+	mov  rdi, number_buffer
+	call atoi
+	call clear_number_buffer
+	ret
+
+clear_number_buffer:
+	push rsi
+	push rcx
+	mov  rcx, 20; 20 characters
+	mov  rsi, number_buffer
+
+.clear_loop:
+	mov byte [rsi], 0x0; Clear byte
+	inc rsi
+	cmp rcx, 0x0; See if rcx is 0
+	je  .exit_loop
+	dec rcx
+	jmp .clear_loop
+
+.exit_loop:
+	pop rcx
+	pop rsi
+	ret
+
+atoi:
+	mov rax, 0; Set initial total to 0
+
+.convert:
+	movzx rsi, byte [rdi]; Get the current character
+	test  rsi, rsi; Check for \0
+	je    .done
+
+	cmp rsi, 0x0a; newline
+	je  .done
+
+	cmp rsi, '0'; Anything less than 0 is invalid
+	jl  .error
+
+	cmp rsi, '9'; Anything greater than 9 is invalid
+	jg  .error
+
+	sub  rsi, 48; Convert from ASCII to decimal
+	imul rax, 10; Multiply total by 10
+	add  rax, rsi; Add current digit to total
+
+	inc rdi; Get the address of the next character
+	jmp .convert
+
+.error:
+	mov rax, -1; Return -1 on error
+
+.done:
+	ret ; Return total or error code
+
+print_uint64:
+	push rax
+	push rsi
+	push rdx
+	mov  r8, 10; Base 10
+
+	mov rax, rdi
+	mov rsi, number_buffer
+	add rsi, 19; Last digit of buffer
+	mov r9, 0x0; Size
+
+.loop:
+	cmp rax, 0x0
+	jle .exit_loop
+	xor rdx, rdx
+	div r8
+	add dl, 48; To ASCII num
+	mov byte [rsi], dl
+	dec rsi
+	inc r9
+	jmp .loop
+
+.exit_loop:
+	mov rax, 0x1
+	mov rdi, 0x1
+	lea rsi, [number_buffer + 20]
+	sub rsi, r9
+	mov rdx, r9
+	syscall
+
+	pop rdx
+	pop rsi
+	pop rax
+
+	ret
+
+section .bss
+arr     resq 1
