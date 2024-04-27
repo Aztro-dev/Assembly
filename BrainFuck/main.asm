32477a54b4ea60c18d2ae57d48a50791bdb28c80
Aztro-dev <davidperezcastellanos@gmail.com>
2024-04-26 22:26:13 -0500

    Uh it actually prints something now

--- BrainFuck/main.asm
+++ BrainFuck/main.asm
@@ -1,22 +1,29 @@
 %define SYS_READ 0x0
-%define STDIN 0x0
 %define SYS_WRITE 0x1
+%define SYS_OPEN 0x2
+%define SYS_CLOSE 0x3
+%define STDIN 0x0
 %define STDOUT 0x1
 %define TAPE_SIZE 30000
+
+section .rodata:
+file_name db "hello_world.bf"
 
 section .data
     tape times TAPE_SIZE db 0x0
     input times 1024 db 0x0
+    file_descriptor dq 1
 
 section .text
 global _start
 
 interpret:
     mov r8, TAPE_SIZE
-    sal r8, 1 ; middle of tape
+    sar r8, 1 ; middle of tape
     mov r9, input
     lea rsi, [tape + r8] ; Pointer
     .loop:
+        inc  r9
         mov al, byte [r9]
         cmp al, 0x0
         je .exit_loop
@@ -62,15 +69,15 @@ interpret:
         .check_open_bracket:
             cmp al, '['
             jne .check_closed_bracket
-            mov al, byte[rsi]
+            mov al, byte[r9]
             cmp al, 0x0
             je .loop ; Skip if *ptr is not true
             mov rcx, 0x1 ; loop counter
             .open_bracket_loop:
                 cmp rcx, 0x0
                 je .loop
-                inc rsi
-                mov al, byte[rsi]
+                inc r9
+                mov al, byte[r9]
                 cmp al, '['
                 jne .open_bracket_loop_closed_bracket
                 inc rcx
@@ -83,15 +90,15 @@ interpret:
         .check_closed_bracket:
             cmp al, ']'
             jne .loop
-            mov al, byte[rsi]
+            mov al, byte[r9]
             cmp al, 0x0
             je .loop ; Skip if *ptr is not true
             mov rcx, 0x1 ; loop counter
             .closed_bracket_loop:
                 cmp rcx, 0x0
                 je .loop
-                inc rsi
-                mov al, byte[rsi]
+                dec r9
+                mov al, byte[r9]
                 cmp al, '['
                 jne .closed_bracket_loop_closed_bracket
                 dec rcx
@@ -105,13 +112,24 @@ interpret:
     ret
 
 _start:
+    mov rax, SYS_OPEN
+    mov rdi, file_name
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+
+    mov qword[file_descriptor], rax
+
     mov rax, SYS_READ
-    mov rdi, STDIN
+    mov rdi, qword[file_descriptor]
     mov rsi, input
     mov rdx, 1024
     syscall
 
     call interpret
+
+    mov rax, SYS_CLOSE
+    mov rdi, qword[file_descriptor]
     
     mov rax, 60
     mov rdi, 0
