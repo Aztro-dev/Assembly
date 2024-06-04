b71142edc12e0dd6533c880232579f909012682d
Aztro-dev <davidperezcastellanos@gmail.com>
2024-06-03 21:36:40 -0500

    I have to restart on the check_hit function smh

--- Graphics/Pong/src/move.asm
+++ Graphics/Pong/src/move.asm
@@ -38,44 +38,11 @@ move_ball:
 	call check_for_hit
 	cmp rax, 0x0
 	je .return
-	mov qword[ball_velocities], 0x0
+	mov qword[ball_velocities], 0x0 ; Stop the ball when colliding
 	.return:
 	ret
 check_for_hit:
-	; https://stackoverflow.com/a/402010
-	movq xmm0, qword[ball_position]
-	movq xmm1, qword[paddle_positions + rdi] ; Offset = the paddle we want to check
-	psubd xmm0, xmm1
-	pabsd xmm0, xmm0
-	movdqu [temp], xmm0
-	cmp dword[temp], PADDLE_WIDTH / 2 + BALL_RADIUS
-	jg .return_false
-	cmp dword[temp + 4], PADDLE_HEIGHT / 2 + BALL_RADIUS
-	jg .return_false
-	cmp dword[temp], PADDLE_WIDTH / 2
-	jle .return_true
-	cmp dword[temp + 4], PADDLE_HEIGHT / 2
-	;jle .return_true
-	jmp .return_false
-	mov eax, dword[temp + 4]
-	mov dword[temp + 8], eax ; For the multiply that is coming later
-	movdqu xmm0, [temp]
-	mov dword[temp], PADDLE_WIDTH / 2
-       	mov dword[temp + 4],PADDLE_HEIGHT / 2
-	psubd xmm0, [temp]
-	pmuldq xmm0, xmm0
-	vpmovqd xmm0, xmm0 ; quadword to dword
-	movdqu [temp], xmm0
-	mov eax, dword[temp]
-	add eax, dword[temp + 4]
-	cmp eax, BALL_RADIUS * BALL_RADIUS
-	;jle .return_true
-	
-	.return_false:
 	xor rax, rax
-	ret
-	.return_true:
-	mov rax, 0x1
 	ret
 section .data
 ; Trust me bro
