2f83bc3a89ce19fb542cdec1de8315937fad9b5b
Aztro-dev <davidperezcastellanos@gmail.com>
2024-10-11 22:17:55 -0500

    fix template.asm

--- CSES/template.asm
+++ CSES/template.asm
@@ -1,12 +1,11 @@
 %define SYS_READ 0
 %define SYS_WRITE 1
-%define SYS_BRK 12
 %define SYS_EXIT 60
 
 %define STDIN 0
 %define STDOUT 1
 
-%define BUF_SIZE 200000
+%define BUF_SIZE 200
 
 section .bss
 input_buffer resb BUF_SIZE
@@ -14,20 +13,29 @@ output_buffer resb BUF_SIZE
 
 section .text
 solve:
+    mov rax, 0x55AA
     ret
 
 global _start
 _start:
+    mov r8, input_buffer
+    mov r9, output_buffer
+    
     mov rax, SYS_READ
     mov rdi, STDIN
-    mov rsi, input_buffer
+    mov rsi, r8
     mov rdx, BUF_SIZE
     syscall
 
     call solve
 
-    mov rdi, rax
     call write_uint64
+
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, r9
+    mov rdx, BUF_SIZE
+    syscall
 
     mov rax, SYS_EXIT
     mov rdi, 0x0
@@ -37,15 +45,15 @@ _start:
 atoi:
     xor rax, rax
     .loop:
-    movzx rcx, byte [rdi]
-    add rdif 1
+    movzx rcx, byte [r8]
+    inc r8
 
     cmp cl, 0x30
     jl .end
     
     ; rax = 10 * rax - '0'
     shl rax, 1		
-    lea rax, [rax+rax*4-48]        
+    lea rax, [rax + rax * 4 -48]        
     ; rax += character
     add rax, rcx
 
@@ -63,26 +71,29 @@ write_uint64:
     .div:
     xor rdx, rdx
     div rcx
-    add rdx, 0x30
+    add rdx, 0x30 ; num % 10 + '0'
 
-    sub rsp, 1
-    mov byte [rsp], dl
+    dec rsp
+    mov byte [rsp], dl ; push character to stack
 
     test rax, rax
-    jnz .div
+    jnz .div ; keep pushing to stack for rest of number
 
+    ; copy stack string to buffer
+    ; we do this to not have to keep track
+    ; of the current position in the buffer
     .loop:
     mov cl, byte [rsp]
-    add rsp, 1
+    inc rsp
 
     mov byte [r9], cl
-    add r9, 1
+    inc r9
 
     cmp rsp, rbp
     jl .loop
 
-    mov byte[r9], 0x20
-    add r9, 1
+    mov byte[r9], 0x20 ; space
+    inc r9
 
     pop rdx
     pop rcx
@@ -92,5 +103,5 @@ write_uint64:
 
 write_newline:
     mov byte [r9], 0x0a
-    add r9, 1
+    inc r9
     ret
