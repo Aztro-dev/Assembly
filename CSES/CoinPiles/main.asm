3daa05822a1c6e9de548efd8670995e099d21f08
Aztro-Dev <davidperezcastellanos@gmail.com>
2024-10-26 23:46:33 -0500

    Start on Coin Piles

---
+++ CSES/CoinPiles/main.asm
@@ -0,0 +1,117 @@
+%define SYS_READ 0
+%define SYS_WRITE 1
+%define SYS_EXIT 60
+
+%define STDIN 0
+%define STDOUT 1
+
+%define BUF_SIZE 200
+
+section .bss
+input_buffer resb BUF_SIZE
+output_buffer resb BUF_SIZE
+
+section .text
+solve:
+    mov rax, 0x55AA
+    ret
+
+global _start
+_start:
+    mov r8, input_buffer
+    mov r9, output_buffer
+    
+    mov rax, SYS_READ
+    mov rdi, STDIN
+    mov rsi, r8
+    mov rdx, BUF_SIZE
+    syscall
+
+    call solve
+    
+    ; Length of output buffer
+    xor r11, r11
+
+    call write_uint64
+
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, output_buffer
+    mov rdx, r11
+    syscall
+
+    mov rax, SYS_EXIT
+    mov rdi, 0x0
+    syscall
+    ret
+
+atoi:
+    xor rax, rax
+    .loop:
+    movzx rcx, byte [r8]
+    inc r8
+
+    cmp cl, 0x30
+    jl .end
+    
+    ; rax = 10 * rax - '0'
+    shl rax, 1		
+    lea rax, [rax + rax * 4 -48]        
+    ; rax += character
+    add rax, rcx
+
+    jmp .loop
+    .end:
+    ret
+write_uint64:
+    push rax
+    push rbp
+    push rcx
+    push rdx
+
+    mov rcx, 10
+    mov rbp, rsp
+    .div:
+    xor rdx, rdx
+    div rcx
+    add rdx, 0x30 ; num % 10 + '0'
+
+    dec rsp
+    mov byte [rsp], dl ; push character to stack
+
+    ; Add bytes created to r11 (length of output buffer)
+    add r11, rbp
+    sub r11, rsp
+
+    test rax, rax
+    jnz .div ; keep pushing to stack for rest of number
+
+    ; copy stack string to buffer
+    ; we do this to not have to keep track
+    ; of the current position in the buffer
+    .loop:
+    mov cl, byte [rsp]
+    inc rsp
+
+    mov byte [r9], cl
+    inc r9
+
+    inc r11
+
+    cmp rsp, rbp
+    jl .loop
+
+    mov byte[r9], 0x20 ; space
+    inc r9
+
+    pop rdx
+    pop rcx
+    pop rbp
+    pop rax
+    ret
+
+write_newline:
+    mov byte [r9], 0x0a
+    inc r9
+    inc r11
+    ret
