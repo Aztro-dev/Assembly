c6a6a1698fbdeb3d03ed27e4800ebdbd6cea302f
Aztro-dev <davidperezcastellanos@gmail.com>
2025-03-20 22:22:41 -0500

    Added malloc version of concat_str

--- Assembler/src/utils.asm
+++ Assembler/src/utils.asm
@@ -7,8 +7,68 @@ global concat_str
 ; output str in rax
 concat_str:
   ; Find length of firststr
+  xor r8, r8 ; length
+  .firststr_len_loop:
+    cmp byte[rdi + r8], 0x0
+    je .exit_firststr_len_loop
+
+    inc r8
+
+    jmp .firststr_len_loop
+
+  .exit_firststr_len_loop:
   ; Find length of secondstr
-  ; Malloc firststr length + secondstr length - 1 (because we don't need two null bytes)
+  xor r9, r9 ; length
+  .secondstr_len_loop:
+    cmp byte[rsi + r9], 0x0
+    je .exit_secondstr_len_loop
+
+    inc r9
+
+    jmp .secondstr_len_loop
+
+  .exit_secondstr_len_loop:
+  ; Malloc firststr length + secondstr length + 1 (because we still need the null byte)
+  add r8, r9
+  inc r8
+  push rdi
+  push rsi
+
+  mov rdi, r8 ; num elements
+  mov rsi, 1  ; each element is 1 byte
+  call malloc
+
+  pop rsi
+  pop rdi
+
+  xor r8, r8
+  .append_firststr:
+    cmp byte[rdi + r8], 0x0
+    je .exit_append_firststr
+
+    mov bl, byte[rdi + r8]
+    mov byte[rax + r8], bl
+
+    inc r8
+
+    jmp .append_firststr
+
+  .exit_append_firststr:
+  xor r9, r9
+  add rax, r8
+
+  .append_secondstr:
+    cmp byte[rsi + r9], 0x0
+    je .exit_append_secondstr
+
+    mov bl, byte[rsi + r9]
+    mov byte[rax + r9], bl
+
+    inc r9
+    jmp .append_secondstr
+
+  .exit_append_secondstr:
+  sub rax, r8
   ; return malloc address
 	ret
 global concat_str_nomalloc
