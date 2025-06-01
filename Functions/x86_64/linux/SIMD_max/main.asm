66496a01f6b78801092c49f9fc1c5bb273de67d7
Aztro-Dev <davidperezcastellanos@gmail.com>
2025-06-01 00:05:58 -0500

    Finish and add verification

--- Functions/x86_64/linux/SIMD_max/main.asm
+++ Functions/x86_64/linux/SIMD_max/main.asm
@@ -1,3 +1,5 @@
+%define VERIFICATION 0 ; Used to verify the output
+
 %define SYS_WRITE 1
 %define SYS_EXIT 60
 
@@ -23,8 +25,23 @@ avx2_uint32_max:
         vpmaxud ymm0, [rdi + rcx * 4]
         inc rcx
         jmp .loop
+    
+    .exit_loop:
+    vextracti128 xmm1, ymm0, 1 ; xmm1 = higher 128 bits of ymm0
+    ; xmm0 already has the lower 128 bits of ymm0
 
-    mov rax, 0x1
+    vpmaxud xmm0, xmm0, xmm1 ; xmm0 = max (lower 128, higher 128)
+    ; leaves 4 values left to combine
+
+    vpshufd xmm1, xmm0, 0b10110001 ; xmm1 = xmm0[0b10, 0b11, 0b00, 0b01] = xmm0[2, 3, 0, 1]
+    vpmaxud xmm0, xmm0, xmm1 ; xmm0 = [max (3, 2), max(2, 3), max(1, 0), max(0, 1) ]
+    ; leaves 2 values left to combine
+
+    vpshufd xmm1, xmm0, 0b01001110 ; xmm1 = xmm0[0b01, 0b00, 0b11, 0b10] = xmm0[1, 0, 3, 2]
+    vpmaxud xmm0, xmm0, xmm1 ; exercise for the reader
+    ; max stored in all slots
+
+    movd eax, xmm0
     ret
 
 global _start
@@ -33,8 +50,13 @@ _start:
 
     mov rdi, numbers
     call avx2_uint32_max
+    
+    %if VERIFICATION
+        push rax
+        call print_newline
+        pop rdi
+    %endif
 
-    mov rdi, rax
     call print_uint64
 
     mov rax, SYS_EXIT
@@ -48,6 +70,17 @@ generate_rand:
         cmp rax, numbers
         jl .exit_loop
         rdrand ebx
+
+        %if VERIFICATION
+            push rax
+            push rbx
+            mov rdi, rbx
+            call print_uint64
+            call print_newline
+            pop rbx
+            pop rax
+        %endif
+
         mov dword[rax], ebx
         sub rax, 0x4
         jmp .loop
@@ -78,7 +111,7 @@ print_uint64:
 
 .exit_loop:
 	mov rax, SYS_WRITE
-	mov rdi, STDIN
+	mov rdi, STDOUT
 	lea rsi, [output_buffer + 20]
 	sub rsi, r9
 	mov rdx, r9
@@ -89,3 +122,16 @@ print_uint64:
 	pop rax
 
 	ret
+
+print_newline:
+    sub rsp, 0x1
+    mov byte[rsp], 0x0a ; newline
+
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, rsp
+    mov rdx, 0x1
+    syscall
+
+    add rsp, 0x1
+    ret
