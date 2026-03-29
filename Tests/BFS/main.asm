cd93f7d8b8f0687dc426963032a5d74c57351059
Aztro-dev <davidperezcastellanos@gmail.com>
2026-03-29 18:34:49 -0500

    Finish board parsing and verification

--- Tests/BFS/main.asm
+++ Tests/BFS/main.asm
@@ -25,19 +25,61 @@ global init_board
 ; rsi: N
 ; rdx: M
 init_board:
+    ; Calculate max index
+    mov rax, rdx
+    imul rsi
+    mov r13, rax ; r13 = N * M
     ; rax = i
     xor rax, rax
+    ; Stores real current index in order to ignore whitespace when parsing
+    xor rcx, rcx
+    ; Store start and end
+    xor r14, r14
+    xor r15, r15
 
     .init_board_parse:
-        mov bh, ' ' ; Empty 
+        ; Exit if we have gone past the max index allowed
+        cmp rcx, r13
+        jge .init_board_exit_parse
+
         mov bl, byte[rdi + rax]
+
+        ; If the character is less than ASCII hashtag, we skip
         cmp bl, '#'
-        cmove bh, bl
-        cmp bl, '#'
-        cmove bh, bl
+        jl .init_board_continue
+
+        ; Store start point in r14
+        cmp bl, 'A'
+        cmove r14, rcx
+
+        ; Store end point in r15
+        cmp bl, 'B'
+        cmove r15, rcx
+
+        ; Copy byte over to board
+        mov byte[board + rcx], bl
+        inc rcx
+
+        .init_board_continue:
+        inc rax
         jmp .init_board_parse
     .init_board_exit_parse:
+    ; Store the start and end for the board in da pointers
+    mov dword[board_start], r14d
+    mov dword[board_end], r15d
     mov rax, board
+    ret
+
+global get_start
+; Returns: board_start
+get_start:
+    mov eax, dword[board_start]
+    ret
+
+global get_end
+; Returns: board_end
+get_end:
+    mov eax, dword[board_end]
     ret
 
 global bfs
--- Tests/BFS/test.c
+++ Tests/BFS/test.c
@@ -1,18 +1,35 @@
+#include <stdint.h>
 #include <stdio.h>
 
 extern char *init_board(char *board_input, int N, int M);
 extern void bfs();
+extern int32_t get_start();
+extern int32_t get_end();
+
+/* input:
+########
+#.A#...#
+#.##.#B#
+#......#
+########
+*/
 
 int main() {
-  char *board_input = "########\n#.A #... #\n#.##.#B #\n#...... #\n########";
+  char *board_input = "########\n#.A#...#\n#.##.#B#\n#......#\n########";
   int N = 5, M = 8;
-  char *board_filtered = init_board(board_input, 5, 8);
+  char *board_filtered = init_board(board_input, N, M);
   for (int i = 0; i < N; i++) {
     for (int ii = 0; ii < M; ii++) {
-      printf("%c", board_filtered[i * N + ii]);
+      printf("%c", board_filtered[i * M + ii]);
     }
     printf("\n");
   }
+
+  int32_t start = get_start();
+  int32_t end = get_end();
+  printf("Start: %d\n", start);
+  printf("End: %d\n", end);
+
   bfs();
 
   return 0;
