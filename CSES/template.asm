02912997c2e5748a8fa0ce07efe02af70198dc98
Aztro-dev <davidperezcastellanos@gmail.com>
2026-03-29 22:41:39 -0500

    Update template.asm to use faster MMAP-powered input

--- CSES/template.asm
+++ CSES/template.asm
@@ -1,31 +1,80 @@
+default REL
+
 %define SYS_READ 0
 %define SYS_WRITE 1
+%define SYS_FSTAT 5
+%define SYS_MMAP 9
 %define SYS_EXIT 60
 
 %define STDIN 0
 %define STDOUT 1
+%define PROT_READ 1
+%define MAP_PRIVATE 2
 
-%define BUF_SIZE 200
+%define OUTPUT_BUF_SIZE 3_000_000
 
 section .bss
-input_buffer resb BUF_SIZE
-output_buffer resb BUF_SIZE
+output_buffer resb OUTPUT_BUF_SIZE
+
+%macro ATOI 1
+    xor %1, %1
+    %%skip_whitespace:
+    movzx r10, byte [r8]
+    inc r8
+    ; skip spaces and tabs and stuff
+    cmp r10b, 0x30
+    jl %%skip_whitespace
+
+    %%loop:
+    ; rax = 10 * rax - '0'
+    lea %1, [%1 + %1 * 4]        
+    ; rax += character - '0'
+    lea %1, [%1 * 2 + r10 - '0']
+
+    movzx r10, byte [r8]
+    inc r8
+    cmp r10b, 0x30
+    jge %%loop
+%endmacro
+
+section .data
+align 2
+; lookup table for fast itoa
+lut100:
+    %assign i 0
+    %rep 100
+        %assign tens (i / 10) + '0' ; tens digit
+        %assign ones (i % 10) + '0' ; ones digit
+        db tens, ones ; write those jawns
+        %assign i i+1
+    %endrep
 
 section .text
 solve:
-    mov rax, 0x55AA
-    ret
+  mov rax, 0x55AA
+  ret
+
 
 global _start
 _start:
-    mov r8, input_buffer
-    mov r9, output_buffer
-    
-    mov rax, SYS_READ
+    ; Use FSTAT to get file size
+    mov rax, SYS_FSTAT
     mov rdi, STDIN
-    mov rsi, r8
-    mov rdx, BUF_SIZE
+    mov rsi, stat_struct
     syscall
+
+    ; File size is at 6 bytes offset of stat struct
+    mov rsi, qword[stat_struct + 48]
+    mov rax, SYS_MMAP
+    xor rdi, rdi
+    mov rdx, PROT_READ ; prot
+    mov r10, MAP_PRIVATE ; flags
+    mov r8, STDIN ; fd = 0
+    xor r9, r9 ; offset = 0
+    syscall
+
+    mov r8, rax
+    mov r9, output_buffer
 
     ; Length of output buffer
     xor r11, r11
@@ -43,72 +92,62 @@ _start:
     syscall
     ret
 
-atoi:
-    xor rax, rax
-    .loop:
-    movzx rcx, byte [r8]
-    inc r8
-
-    cmp cl, 0x30
-    jl .end
-    
-    ; rax = 10 * rax - '0'
-    shl rax, 1		
-    lea rax, [rax + rax * 4 - 48]        
-    ; rax += character
-    add rax, rcx
-
-    jmp .loop
-    .end:
-    ret
-write_uint64:
+write_uint32:
     push rax
     push rbp
     push rcx
     push rdx
+    push r8
 
-    mov rcx, 10
     mov rbp, rsp
-    .div:
-    xor rdx, rdx
-    div rcx
-    add rdx, 0x30 ; num % 10 + '0'
+    ; 16 byte playspace thingy
+    sub rsp, 16
+    ; point to end of buffer skull emoji
+    lea r8, [rbp]
 
-    dec rsp
-    mov byte [rsp], dl ; push character to stack
+    .div100:
+    mov r10, rax
 
+    ; super duper fast divide new and improved
+    ; divide by 100
+    imul rax, rax, 1374389535 
+    shr rax, 37
+    ; This part is to find trhe remainder
+    mov rcx, rax          
+    imul rcx, rcx, 100
+    sub r10, rcx
+    
+    ; lookup table my beloved (get 2 characters at the same time)
+    mov dx, word [lut100 + r10 * 2]
+    sub r8, 2
+    mov word [r8], dx
+    
     test rax, rax
-    jnz .div ; keep pushing to stack for rest of number
+    jnz .div100
 
-    ; Add bytes created to r11 (length of output buffer)
-    add r11, rbp
-    sub r11, rsp
+    ; Make sure leading zeroes don't affect anything pls
+    cmp byte [r8], '0'
+    jne .no_leading_zero
+    inc r8
+    .no_leading_zero:
 
-    ; copy stack string to buffer
-    ; we do this to not have to keep track
-    ; of the current position in the buffer
-    .loop:
-    mov cl, byte [rsp]
-    inc rsp
+    ; branchless string copy
+    mov r10, rbp
+    sub r10, r8
+    mov rax, [r8]            
+    mov [r9], rax            
+    mov ax, [r8 + 8]         
+    mov [r9 + 8], ax
 
-    mov byte [r9], cl
-    inc r9
+    add r9, r10
 
-    cmp rsp, rbp
-    jl .loop
+    ; update final size so we print the right number of chars
+    lea r11, [r11 + r10]
 
-    mov byte[r9], 0x20 ; space
-    inc r9
-    inc r11
-
+    mov rsp, rbp
+    pop r8
     pop rdx
     pop rcx
     pop rbp
     pop rax
-    ret
-
-write_newline:
-    mov byte [r9], 0x0a
-    inc r9
-    inc r11
     ret
